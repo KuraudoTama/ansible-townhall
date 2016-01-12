0485c01d79bba4cd64d3b8d9e9e9d522954429cf
joshuakwan <garfilone@gmail.com>
2016-01-12 22:34:42 +0800

    finish the list and creation pages

--- web_ui/views.py
+++ web_ui/views.py
@@ -3,5 +3,10 @@ from flask import render_template
 
 
 @ui.route('/ui')
-def ui():
+def ui_index():
     return render_template('template_index.html')
+
+
+@ui.route('/ui/jobs/create')
+def ui_create_jobs():
+    return render_template('template_job_create.html')
