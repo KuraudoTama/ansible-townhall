59529f8b479b23d526d6b35dacd00b97134eb89a
LaraMurri <batman1984super@163.com>
2016-01-12 15:30:18 +0800

    Add two new interfaces: get_total_counts_grouped_by_categories get_task_duration_grouped_by_names

--- job_persistence/ansible_job_db.py
+++ job_persistence/ansible_job_db.py
@@ -37,6 +37,46 @@ class JobPersistence(object):
         inserted_doc_dict["_id"] = str(inserted_doc.inserted_id)
         return inserted_doc_dict
     
+    def get_total_counts_grouped_by_categories(self, job_id):
+        dataset = {}
+        pipeline = [{"$match" : {"job_id" : job_id}},
+                    {"$group" : {"_id" : "$category", "count" : {"$sum" : 1}}}
+                    ]
+        cursor = self.log_collection.aggregate(pipeline, cursor={})
+        for item in cursor:
+            key = item["_id"]
+            value = item["count"]
+            dataset[key] = value
+        
+        if not dataset:
+            raise RecordNotFoundException("Record is not found by the job id '%s'" % job_id)
+        logger.info("DB Level-Total counts grouped by categories are:%s" % str(dataset))
+        return dataset
+    
+    def get_task_duration_grouped_by_names(self, job_id, size):
+        data_list = []
+        dataset = {}
+        pipeline = [
+                    {"$match" : {"job_id" : job_id}},
+                    {"$group" : {"_id" : "$task_name", "duration" : {"$sum" : "$task_duration"}}},
+                    {"$sort" : {"duration" : -1}}
+                    ]
+        log_set = self.log_collection.aggregate(pipeline, cursor={})
+        i=1
+        for item in log_set:
+            if i > size:
+                break
+            key = item["_id"]
+            value = item["duration"]
+            data_list.append({key:value})
+            i = i+1
+        
+        if not data_list:
+            raise RecordNotFoundException("Record is not found by the job id '%s'" % job_id)
+        dataset["task_duration_list"] = data_list
+        logger.info("DB Level-Total task duration grouped by task names are:%s" % str(dataset))
+        return dataset
+    
     def update_job_status(self, job_id, job_status):
         self.job_collection.update_one(
                                        {"_id" : ObjectId(job_id)},
